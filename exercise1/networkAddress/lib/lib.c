c87b8a86b7e38d515bae24fc6b15c88a3df2df05
taitesen <iamhmunhlei@gmail.com>
2024-10-07 00:12:46 +0530

    rewrite the lib

--- exercise1/networkAddress/lib/lib.c
+++ exercise1/networkAddress/lib/lib.c
@@ -7,38 +7,15 @@ void get_network_id(char *ip_addr, char mask, char *output_buffer) {
         fprintf(stderr, "Invalid IPv4 Address\n");
         return;
     }
-    int k = 32 - (int)mask;
-    if(k < 1 || k > 31) {
-        fprintf(stderr, "Invalid Mask\n");
-        return;
-    }
-    if(k <= 8) {
-        octet4 = octet4 & (0 << k);
-        sprintf(output_buffer, "%d.%d.%d.%d", octet1, octet2, octet3, octet4);
-        return;
-    }
-    if(k <= 16) {
-        k -= 8;
-        octet4 = 0;
-        octet3 = octet3 & (0 << k);
-        sprintf(output_buffer, "%d.%d.%d.%d", octet1, octet2, octet3, octet4);
-        return;
-    }
-    if(k <= 24) {
-        k -= 16;
-        octet4 = 0;
-        octet3 = 0;
-        octet2 = octet2 & (0 << k);
-        sprintf(output_buffer, "%d.%d.%d.%d", octet1, octet2, octet3, octet4);
-        return;
-    }
-    if(k <= 31) {
-        k -= 24;
-        octet4 = 0;
-        octet3 = 0;
-        octet2 = 0;
-        octet1 = octet1 & (0 << k);
-        sprintf(output_buffer, "%d.%d.%d.%d", octet1, octet2, octet3, octet4);
-        return;
-    }
+    // Subnet mask in integer
+    unsigned int SubnetMaskInt = (mask == 32) ? 0xFFFFFFFF : (0xFFFFFFFF << (32 - mask));
+    unsigned int IpAddressInt = octet1 << 24 | octet2 << 16 | octet3 << 8 | octet4;
+    unsigned int NetworkIdInt = SubnetMaskInt & IpAddressInt;
+
+    octet1 = (NetworkIdInt >> 24) & 0xFF; // extract first octet from NetworkID
+    octet2 = (NetworkIdInt >> 16) & 0xFF; // extract second octet from NetworkID
+    octet3 = (NetworkIdInt >> 8) & 0xFF;  // extract third octet from NetworkID
+    octet4 = NetworkIdInt & 0xFF;         // extract fourth octet from NetworkID
+
+    sprintf(output_buffer, "%d.%d.%d.%d", octet1, octet2, octet3, octet4);
 }
