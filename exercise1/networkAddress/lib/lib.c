b12d022c3ff87f799ead06353b2ada3510b3e6c0
taitesen <iamhmunhlei@gmail.com>
2024-10-07 00:19:59 +0530

    makes it more readible

--- exercise1/networkAddress/lib/lib.c
+++ exercise1/networkAddress/lib/lib.c
@@ -7,15 +7,17 @@ void get_network_id(char *ip_addr, char mask, char *output_buffer) {
         fprintf(stderr, "Invalid IPv4 Address\n");
         return;
     }
-    // Subnet mask in integer
+    // In order to fetch NetworkID integer value,
+    // We convert Subnet Mask and IPv4 Address into integer value.
     unsigned int SubnetMaskInt = (mask == 32) ? 0xFFFFFFFF : (0xFFFFFFFF << (32 - mask));
     unsigned int IpAddressInt = octet1 << 24 | octet2 << 16 | octet3 << 8 | octet4;
     unsigned int NetworkIdInt = SubnetMaskInt & IpAddressInt;
 
-    octet1 = (NetworkIdInt >> 24) & 0xFF; // extract first octet from NetworkID
-    octet2 = (NetworkIdInt >> 16) & 0xFF; // extract second octet from NetworkID
-    octet3 = (NetworkIdInt >> 8) & 0xFF;  // extract third octet from NetworkID
-    octet4 = NetworkIdInt & 0xFF;         // extract fourth octet from NetworkID
+    // We need to extract their respective octet value(8-bit/0xFF) from NetworkID
+    octet1 = (NetworkIdInt >> 24) & 0xFF;
+    octet2 = (NetworkIdInt >> 16) & 0xFF;
+    octet3 = (NetworkIdInt >> 8) & 0xFF;
+    octet4 = NetworkIdInt & 0xFF;
 
     sprintf(output_buffer, "%d.%d.%d.%d", octet1, octet2, octet3, octet4);
 }
